8b3ef2d5a27f06535470497504e0ef559468cfcd
snehaweb <web.sdiwan@gmail.com>
2022-11-05 21:59:38 -0400

    added styles for image

--- frontend/src/screens/Error404Screen.js
+++ frontend/src/screens/Error404Screen.js
@@ -1,6 +1,7 @@
 const Error404Screen ={
     render:() =>{
-        return '<div >Page not found! </div>';
+        return `<div class="notfound">
+         <img src=${"https://th.bing.com/th/id/OIP.5nlcXCGojRLtNcNLjuVmFQHaFP?w=239&h=180&c=7&r=0&o=5&pid=1.7"} /> </div>`;
     },
 };
 
